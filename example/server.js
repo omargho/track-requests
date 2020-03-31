4d250d1eb7ab7bf0a59987f20705642cbe53a752
Ahmed Makni <hmed.mak96@gmail.com>
2020-03-31 16:00:55 +0100

    add endpointsBodies option

--- example/server.js
+++ example/server.js
@@ -4,45 +4,14 @@ const trackRequests = require('..');
 const trackRequestsOptions = {
   elasticSearchOptions: {
     elasticSearchUrl: 'http://127.0.0.1:9200/'
-  },
-  endpointsBodies:[
-    {
-      endpoint : '/1',
-      body : {
-        name: {"type" : "string"},
-        email: {"type" : "string"}
-      }
-    },
-    {
-      endpoint : '/',
-      body : {
-        url: {"type" : "string"},
-        email: {"type" : "string"}
-      }
-    }
-  ]
-
+  }
 };
-
-app.use(express.json());
-
-
 app.use(trackRequests(trackRequestsOptions));
 
-app.post('/', function(req, res) {
+app.get('/', function(req, res) {
   res.send('Hello World!');
 });
-
-app.post('/1', function(req, res) {
-  res.send('Hello 1!');
-});
-app.post('/2', function(req, res) {
-  res.send('Hello 2!');
-});
-
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
 });
-
-
